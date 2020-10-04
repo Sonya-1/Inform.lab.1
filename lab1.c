bf895002faa8bf1fa2934705ca3ff128c97e1897
Sonya-1 <71211281+Sonya-1@users.noreply.github.com>
2020-10-04 13:48:22 +0300

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -2,19 +2,43 @@
 
 int main()
 {
-	int n, max_n;
-	max_n = 0;
+	int n, max_n = 0, min_n = 0, premax = 0, premin = 0;
 
-	do {
-		printf("Input number: ");
+	while (1) 
+	{
+		printf("Input n ");
 		scanf("%d", &n);
-		if (max_n <= n) {
-			max_n = n;
+
+		if (n == 0) {
+			break;
 		}
 
-	} while (n != 0);
+		if (max_n == 0 || max_n < n) {
+			premax = max_n;
+			max_n = n;
+		}
+		else if (n > premax) {
+			premax = n;
+		}
+
+		if (min_n == 0 || n < min_n) {
+			premin = min_n;
+			min_n = n;
+		}
+		else if (premin == 0 || n < premin) {
+			premin = n;
+		}
+	}
+
+	if (premax == 0) {
+		printf("No data\n");
+		return 0;
+	}
 
 	printf("Max %d\n", max_n);
+	printf("Min %d\n", min_n);
+	printf("PreMax %d\n", premax);
+	printf("PreMin %d\n", premin);
 
 	return 0;
 }
